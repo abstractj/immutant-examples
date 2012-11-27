e4941d6c54377f1307cfb706dd1a3734388eeb60
Bruno Oliveira <bruno@abstractj.org>
2012-11-27 14:10:36 -0200

    Messaging examples

--- immutant.clj
+++ immutant.clj
@@ -2,7 +2,7 @@
 
 (ns beer.init
   (:use beer.core)
-  (:require [immutant.messaging :as messaging]
+  (:require [immutant.messaging :as msg]
             [immutant.web :as web]))
 
 (defn beer-handler [request]
@@ -12,22 +12,11 @@
 
 (web/start "/" beer-handler)
 
-
-;; This file will be loaded when the application is deployed to Immutant, and
-;; can be used to start services your app needs. Examples:
-
-
-;; Web endpoints need a context-path and ring handler function. The context
-;; path given here is a sub-path to the global context-path for the app
-;; if any.
-
-; (web/start "/" my-ring-handler)
-; (web/start "/foo" a-different-ring-handler)
-
-;; To start a Noir app:
-; (server/load-views (str (web/src-dir) "/beer/views"))
-; (web/start "/" (server/gen-handler {:mode :dev :ns 'beer}))
-
+;; Messaging
+(def myqueue "/queue/myqueue")
+(msg/start myqueue)
+(msg/listen myqueue #(println "received: " %))
+(msg/publish myqueue "This is polyglot love")
 
 ;; Messaging allows for starting (and stopping) destinations (queues & topics)
 ;; and listening for messages on a destination.
