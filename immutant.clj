219fb3eab54e29dc129d18e0d6512217031a65a5
Bruno Oliveira <bruno@abstractj.org>
2012-11-27 14:32:58 -0200

    Daemonizing services

--- immutant.clj
+++ immutant.clj
@@ -3,7 +3,8 @@
 (ns beer.init
   (:use beer.core)
   (:require [immutant.messaging :as msg]
-            [immutant.web :as web]))
+            [immutant.web :as web]
+            [immutant.daemons :as daemon]))
 
 (defn beer-handler [request]
   {:status 200
@@ -16,7 +17,29 @@
 (def myqueue "/queue/myqueue")
 (msg/start myqueue)
 (msg/listen myqueue #(println "received: " %))
-(msg/publish myqueue "This is polyglot love")
+
+(defn listener
+  "Starting the daemon"
+  []
+  (msg/publish myqueue "This is polyglot love"))
+
+;; Daemon
+(def done (atom false))
+
+(defrecord BeerService []
+  daemon/Daemon
+  (start [_]
+    (reset! done false)
+    (loop [i 0]
+      (Thread/sleep 1000)
+      (when-not @done
+        (listener)
+        (recur (inc i)))))
+  (stop [_] (reset! done true)))
+
+;; Register the daemon
+(daemon/create "beerdaemon" (BeerService.) :singleton true)
+
 
 ;; Messaging allows for starting (and stopping) destinations (queues & topics)
 ;; and listening for messages on a destination.
